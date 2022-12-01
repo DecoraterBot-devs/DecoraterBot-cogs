bb18f497d254a3a562659e480b9da5b5181fed90
AraHaan <seandhunt_7@yahoo.com>
2022-12-01 03:27:22 -0500

    Load the credits.json file using async.

--- credits.py
+++ credits.py
@@ -12,21 +12,19 @@ class Credits(commands.Cog):
     """
     Credits Commands Plugin Class.
     """
-    def __init__(self):
-        self.credits: BaseConfigReader = BaseConfigReader(file="credits.json")
 
     @app_commands.command(name='credits', description='Gives the user 500 daily credits.')
     async def credits_command(self, interaction: discord.Interaction):
         """
         /credits Command for DecoraterBot.
         """
-        current_credits = 0
-        try:
-            current_credits = self.credits[str(interaction.user.id)]['credits']
-        except (KeyError, TypeError):
-            pass
-        self.credits[str(interaction.user.id)]['credits'] = current_credits + 500
-        self.credits.save()
+        current_credits: int = 0
+        async with BaseConfigReader(file='credits.json') as _credits:
+            try:
+                current_credits = _credits[str(interaction.user.id)]['credits']
+            except (KeyError, TypeError):
+                pass
+            _credits[str(interaction.user.id)]['credits'] = current_credits + 500
         await interaction.response.send_message(
                 f':atm:  |  {interaction.user.name}, you received your :dollar: 500 daily credits!')
 
@@ -40,21 +38,22 @@ class Credits(commands.Cog):
         """
         /givecredits Command for DecoraterBot.
         """
-        current_credits = 0
-        current_credits2 = 0
-        try:
-            current_credits = self.credits[str(interaction.user.id)]['credits']
-        except (KeyError, TypeError):
-            pass
-        try:
-            current_credits2 = self.credits[str(user.id)]['credits']
-        except (KeyError, TypeError):
-            pass
+        current_credits: int = 0
+        current_credits2: int = 0
+        async with BaseConfigReader(file='credits.json') as _credits:
+            try:
+                current_credits = _credits[str(interaction.user.id)]['credits']
+            except (KeyError, TypeError):
+                pass
+            try:
+                current_credits2 = _credits[str(user.id)]['credits']
+            except (KeyError, TypeError):
+                pass
+            if -1 < creditnum < current_credits:
+                _credits[str(interaction.user.id)]['credits'] = current_credits - creditnum
+                _credits[str(user.id)]['credits'] = current_credits2 + creditnum
         if creditnum > -1:
             if current_credits > creditnum:
-                self.credits[str(interaction.user.id)]['credits'] = current_credits - creditnum
-                self.credits[str(user.id)]['credits'] = current_credits2 + creditnum
-                self.credits.save()
                 await interaction.response.send_message(
                     f'{interaction.user.name}, you have transferred :dollar: {creditnum} credits to {user.name}!')
             else:
@@ -68,11 +67,12 @@ class Credits(commands.Cog):
         """
         /balance Command for DecoraterBot.
         """
-        current_credits = 0
-        try:
-            current_credits = self.credits[str(interaction.user.id)]['credits']
-        except (KeyError, TypeError):
-            pass
+        current_credits: int = 0
+        async with BaseConfigReader(file='credits.json') as _credits:
+            try:
+                current_credits = _credits[str(interaction.user.id)]['credits']
+            except (KeyError, TypeError):
+                pass
         await interaction.response.send_message(
             f'{interaction.user.name}, you have :dollar: {current_credits} credits!')
 
