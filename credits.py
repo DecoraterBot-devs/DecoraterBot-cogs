deb523c16c67c057153af09d35377ebe52212a3a
AraHaan <seandhunt_7@yahoo.com>
2022-12-01 01:01:55 -0500

    Rework nsfw command and credits.py.

--- credits.py
+++ credits.py
@@ -5,14 +5,15 @@ credits Plugin for DecoraterBot.
 import discord
 from discord import app_commands
 from discord.ext import commands
+from DecoraterBotUtils.readers import BaseConfigReader
 
 
 class Credits(commands.Cog):
     """
     Credits Commands Plugin Class.
     """
-    def __init__(self, bot):
-        self.bot = bot
+    def __init__(self):
+        self.credits: BaseConfigReader = BaseConfigReader(file="credits.json")
 
     @app_commands.command(name='credits', description='Gives the user 500 daily credits.')
     async def credits_command(self, interaction: discord.Interaction):
@@ -21,12 +22,11 @@ class Credits(commands.Cog):
         """
         current_credits = 0
         try:
-            current_credits = self.bot.credits.getcredits(
-                str(interaction.user.id), 'credits')
+            current_credits = self.credits[str(interaction.user.id)]['credits']
         except (KeyError, TypeError):
             pass
-        self.bot.credits.setcredits(
-            str(interaction.user.id), 'credits', current_credits + 500)
+        self.credits[str(interaction.user.id)]['credits'] = current_credits + 500
+        self.credits.save()
         await interaction.response.send_message(
                 f':atm:  |  {interaction.user.name}, you received your :dollar: 500 daily credits!')
 
@@ -43,23 +43,18 @@ class Credits(commands.Cog):
         current_credits = 0
         current_credits2 = 0
         try:
-            current_credits = self.bot.credits.getcredits(
-                str(interaction.user.id), 'credits')
+            current_credits = self.credits[str(interaction.user.id)]['credits']
         except (KeyError, TypeError):
             pass
         try:
-            current_credits2 = self.bot.credits.getcredits(
-                str(user.id), 'credits')
+            current_credits2 = self.credits[str(user.id)]['credits']
         except (KeyError, TypeError):
             pass
         if creditnum > -1:
             if current_credits > creditnum:
-                self.bot.credits.setcredits(
-                    str(interaction.user.id), 'credits',
-                    current_credits - creditnum)
-                self.bot.credits.setcredits(
-                    str(user.id), 'credits',
-                    current_credits2 + creditnum)
+                self.credits[str(interaction.user.id)]['credits'] = current_credits - creditnum
+                self.credits[str(user.id)]['credits'] = current_credits2 + creditnum
+                self.credits.save()
                 await interaction.response.send_message(
                     f'{interaction.user.name}, you have transferred :dollar: {creditnum} credits to {user.name}!')
             else:
@@ -75,8 +70,7 @@ class Credits(commands.Cog):
         """
         current_credits = 0
         try:
-            current_credits = self.bot.credits.getcredits(
-                str(interaction.user.id), 'credits')
+            current_credits = self.credits[str(interaction.user.id)]['credits']
         except (KeyError, TypeError):
             pass
         await interaction.response.send_message(
@@ -87,4 +81,4 @@ async def setup(bot):
     """
     DecoraterBot's Credits Plugin.
     """
-    await bot.add_cog(Credits(bot))
+    await bot.add_cog(Credits())
--- nsfw.py
+++ nsfw.py
@@ -6,7 +6,6 @@ import discord
 from discord import app_commands
 from discord.ext import commands
 import nsfw_dl
-from nsfw_dl import errors
 
 
 class NSFW(commands.Cog):
@@ -15,31 +14,41 @@ class NSFW(commands.Cog):
     """
 
     @app_commands.command(
-        name='rule34',
-        description='Searches rule34 for some images.',
+        name='nsfw',
+        description='Searches a service for some nsfw images.',
         nsfw=True)
-    @app_commands.describe(searchterm='The tag to search with.')
-    async def rule34_command(self, interaction: discord.Interaction, searchterm: str = ''):
+    @app_commands.describe(
+        name='The name of the service to look for the image from.',
+        searchterm='The tag to search with.')
+    async def nsfw_command(self, interaction: discord.Interaction, name: str = '', searchterm: str = ''):
         """
-        /rule34 Search Command for DecoraterBot.
+        /nsfw Search Command for DecoraterBot for specific loaders.
         """
         await interaction.response.defer(thinking=True)
-        if searchterm != '':
-            try:
-                with nsfw_dl.NSFWDL() as dl:
-                    await self.image_helper(
-                        dl.download(
-                            "Rule34Search",
-                            args=searchterm),
-                        interaction)
-            except nsfw_dl.errors.NoResultsFound:
-                await interaction.followup.send(
-                    content='Sorry, could not find an image from the search.')
-        else:
-            with nsfw_dl.NSFWDL() as dl:
-                await self.image_helper(
-                    dl.download("Rule34Random"),
-                    interaction)
+        loader = f'{name.capitalize()}Search' if searchterm != '' else f'{name.capitalize()}Random'
+        if name in nsfw_dl.SOURCES.keys():
+            if loader not in nsfw_dl.SOURCES[name]:
+                searchterm = ''
+                loader = loader.replace('Search', 'Random')
+        await self.nsfw_helper(interaction, loader, searchterm)
+
+    async def nsfw_helper(self, interaction: discord.Interaction, loader: str, searchterm: str):
+        try:
+            if searchterm != '':
+                async with nsfw_dl.NSFWDL() as dl:
+                    await self.image_helper(await dl.download(loader, args=searchterm), interaction)
+            else:
+                async with nsfw_dl.NSFWDL() as dl:
+                    await self.image_helper(await dl.download(loader), interaction)
+        except nsfw_dl.errors.NoResultsFound:
+            await interaction.followup.send(
+                content='Sorry, could not find an image from the search.')
+        except nsfw_dl.errors.NoLoader:
+            sources = ''
+            for source in nsfw_dl.SOURCES.keys():
+                sources += f'- {source}\n'
+            await interaction.followup.send(
+                content=f'name must be one of:\n{sources}')
 
     @staticmethod
     async def image_helper(image, interaction: discord.Interaction):
